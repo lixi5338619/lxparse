ca97e8ef2bc2ad464a5dad7e7e220a863e4d4f37
李玺 <125066648@qq.com>
2022-08-22 17:04:00 +0800

    content parse

--- lxparse/parsepack/parse_content.py
+++ lxparse/parsepack/parse_content.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
-
 from lxparse.readability import Document
 from lxml import etree
+from html import unescape
 
 class ContentExtractor:
     def extractor(self, html, content_xpath):
         element = etree.HTML(html)
         if content_xpath:
-            content = ''.join(element.xpath(content_xpath))
-            return content
+            body = element.xpath(content_xpath)
+            if body:
+                content = unescape(etree.tostring(body[0], encoding='utf-8').decode())
+                return content
         doc = Document(html)
         return doc.summary()
